894d4d48b6bb02b398437590794c2d5014b4ae35
james <jguest777@gmail.com>
2020-05-29 16:10:36 -0400

    new seeds to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,9 @@
-
+9.times do |i|
+  Question.create(
+    description: "Question #{i + 1}",
+    answer: "This is the #{i + 1} correct answer!",
+    bad_answer_one: "Not the #{i + 1} right answer.",
+    bad_answer_two: "This is also not the #{i + 1} right answer.",
+    category: 'sports'
+  )
+end
