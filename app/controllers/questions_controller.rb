97d7a62652102ba417110920a4b71b0183f9bf8f
james <jguest777@gmail.com>
2020-05-29 15:09:24 -0400

    installed simple_form and completed form test data

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,19 +1,26 @@
 class QuestionsController < ApplicationController
-    before_action :authenticate_user!, only: [:index]
+    before_action :authenticate_user!
   
   def index
-
     @question = Question.order("RANDOM()").first
-    
   end
 
   def new
-    
+    @question = Question.new
+  end
+
+  def create
+    Question.create(questions_params)
+    redirect_to new_question_path
+  end
+
+  def show
+    # @question = Question.find(params[:id])
   end
 
   private
 
   def questions_params
-    params.require(:question).permit(:description, :answer)
+    params.require(:question).permit(:description, :answer, :bad_answer_one, :bad_answer_two, :category)
   end
 end
