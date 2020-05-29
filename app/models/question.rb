c4f232a541fc9019b8ab3baeb1a7ab128d651494
james <jguest777@gmail.com>
2020-05-29 15:48:33 -0400

    db seed

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,7 @@
 class Question < ApplicationRecord
+  validates :description, presence: true
+  validates :answer, presence: true
+  validates :bad_answer_one, presence: true
+  validates :bad_answer_two, presence: true
+  validates :category, presence: true
 end
