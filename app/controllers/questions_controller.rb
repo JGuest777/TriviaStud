f0af0e03b0723de2910e2477f8c3453fe261fda1
james <jguest777@gmail.com>
2020-05-28 20:17:20 -0400

    test question render on index

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,6 +2,10 @@ class QuestionsController < ApplicationController
     before_action :authenticate_user!, only: [:index]
   
   def index
+    @trivia = Trivium.all
+  end
+
+  def new
     
   end
 end
