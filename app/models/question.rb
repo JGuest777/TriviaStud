0854f4e9b3222d5c94d8a43811947839f86369d6
james <jguest777@gmail.com>
2020-05-30 00:08:28 -0400

    submit form functional besides tags

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,11 +1,11 @@
 class Question < ApplicationRecord
-  belongs_to :user
+  has_many :users
 
   validates :description, presence: true, length: { maximum: 300, minimum: 3 }
   validates :answer, presence: true, length: { maximum: 300, minimum: 3 }
   validates :bad_answer_one, presence: true, length: { maximum: 300, minimum: 3 }
   validates :bad_answer_two, presence: true, length: { maximum: 300, minimum: 3 }
-  validates :category, presence: true, length: { maximum: 300, minimum: 3 }
+  validates :category, presence: true
 
   acts_as_taggable_on :tags
 
