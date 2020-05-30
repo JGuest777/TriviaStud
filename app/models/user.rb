cdb25fee3df0e2439d84292d28f42832e7a18150
james <jguest777@gmail.com>
2020-05-30 19:29:42 -0400

    user model reconfig for heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,12 @@
 class User < ApplicationRecord
-  attr_accessor :login
+  attr_writer :login
   has_many :questions
   
   validates :username, presence: :true, uniqueness: { case_sensitive: false }
   validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
-  # validate :validate_username
   
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, authentication_keys: [:login]
+         :recoverable, :rememberable, :trackable, :validatable, authentication_keys: [:login]
 
   def login
     @login || self.username || self.email
@@ -19,6 +18,12 @@ class User < ApplicationRecord
 	    where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
 	  else
 	    where(conditions).first
+	  end
+  end
+
+  def validate_username
+	  if User.where(email: username).exists?
+	    errors.add(:username, :invalid)
 	  end
   end
   
