a10e181f1db3db049e661cf37f2f2dd02309e91f
james <jguest777@gmail.com>
2020-05-30 22:32:59 -0400

    removed user display from index

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -16,7 +16,7 @@ class QuestionsController < ApplicationController
   end
 
   def new
-    @question = Question.new
+    @questions = Question.new
   end
 
   def create
@@ -29,7 +29,7 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    @question = Question.find(params[:id])
+    @questions = Question.find(params[:id])
   end
 
   private
